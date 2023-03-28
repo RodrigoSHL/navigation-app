3058b35e21938a96985083a35a8e1e7ca0890c13
RodrigoSHL <shl.catalan@gmail.com>
2023-03-27 23:47:25 -0300

    FEAT: Pag4
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/navigator/StackNavigation.tsx
+++ src/navigator/StackNavigation.tsx
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Pag1 from '../screen/Pag1';
 import Pag2 from '../screen/Pag2';
 import Pag3 from '../screen/Pag3';
+import Pag4 from '../screen/Pag4';
 
 const Stack = createStackNavigator();
 
@@ -11,7 +12,7 @@ export const StackNavigator = () => {
       <Stack.Screen name="Pag1" component={Pag1} />
       <Stack.Screen name="Pag2" component={Pag2} />
       <Stack.Screen name="Pag3" component={Pag3} />
-
+      <Stack.Screen name="Pag4" component={Pag4} />
     </Stack.Navigator>
   );
 }
---
+++ src/screen/Pag4.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Pag4 = () => {
+  return (
+    <View><Text>Pag4</Text></View>
+  )
+}
+
+export default Pag4
